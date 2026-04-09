8c7b63aa77ee9d4aa8dd36280701811be64d712c
bharat008-star <https://github.com/bharat008-star/HTML-ECE1.git>
2026-04-09 12:24:29 +0530

    function use

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import greeting from './greeting'
 function App() {
   
 
   return (
     <>
-      <h1>Welcome to Vite !!!!</h1>
+      <greeting />
     </>
   )
 }
---
+++ vite-project/src/greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function greeting() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default greeting
